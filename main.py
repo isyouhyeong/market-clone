b7481dc3ba09d2e692ecdc8a900af76c5f6a7d9b
aiden_Lee <isyouh77@gmail.com>
2024-06-20 22:15:41 +0900

    update

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
+import sqlite3
+
+con = sqlite3.connect("db.db", check_same_thread=False)
+cur = con.cursor()
 
 app = FastAPI()
 
 
 @app.post("/items")
-def create_item(
+async def create_item(
     image: UploadFile,
     title: Annotated[str, Form()],
     price: Annotated[int, Form()],
-    discription: Annotated[str, Form()],
+    description: Annotated[str, Form()],
     place: Annotated[str, Form()],
 ):
-    print(image, title, price, discription, place)
+
+    image_bytes = await image.read()
+    cur.execute(
+        f"""
+                  INSERT INTO items(title, image, price, description, place)
+                  VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
+                  """
+    )
+    con.commit()
     return "200"
 
 
