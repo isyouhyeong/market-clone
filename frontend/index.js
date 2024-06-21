7c236e3063da09cdb2351dce24f8b09898399f5f
aiden_Lee <isyouh77@gmail.com>
2024-06-21 20:22:54 +0900

    update

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+const fetchList = async () => {
+    const res = await fetch('/items');
+    const data = await res.json();
+    console.log(data);
+    
+};
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 from typing import Annotated
 import sqlite3
 
@@ -16,17 +17,29 @@ async def create_item(
     price: Annotated[int, Form()],
     description: Annotated[str, Form()],
     place: Annotated[str, Form()],
+    insertAt: Annotated[int, Form()],
 ):
 
     image_bytes = await image.read()
     cur.execute(
         f"""
-                  INSERT INTO items(title, image, price, description, place)
-                  VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
+                  INSERT INTO 
+                  items(title, image, price, description, place, insertAt)
+                  VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}', {insertAt})
                   """
     )
     con.commit()
     return "200"
+
+
+@app.get("/items")
+async def get_items():
+    rows = cur.execute(
+        f"""
+                       SELECT * FROM items;
+                       """
+    ).fetchall()
+    return JSONResponse
 
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
